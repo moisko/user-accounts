b5362299e26c84c9bfb3ce21bb9acd4ec89dae5e
moisko <boris.r.borisov@gmail.com>
2015-11-06 17:51:10 +0200

    rearrange methods in AccountDAO
    Change-Id: I8e0a36d57b5023a126ce2e3b95726a5bf5e51d68

--- src/main/java/accounts/dao/AccountDAO.java
+++ src/main/java/accounts/dao/AccountDAO.java
@@ -35,14 +35,6 @@ public class AccountDAO {
 		}
 	}
 
-	@SuppressWarnings("unchecked")
-	private List<Account> getAccountsFromDb(EntityManager em, int maxResults) {
-		Query q = em.createNamedQuery("getAllAccounts");
-		q.setMaxResults(maxResults);
-		List<Account> allAccounts = (List<Account>) q.getResultList();
-		return allAccounts;
-	}
-
 	public Account getAccount(Long id) {
 		EntityManager em = emf.createEntityManager();
 		try {
@@ -52,13 +44,6 @@ public class AccountDAO {
 		}
 	}
 
-	private Account getAccountFromDb(EntityManager em, Long id) {
-		Query q = em.createNamedQuery("getAccountById");
-		q.setParameter("id", id);
-		Account account = (Account) q.getSingleResult();
-		return account;
-	}
-
 	public void createAccount(Account account) {
 		AccountValidator.validate(account);
 		EntityManager em = emf.createEntityManager();
@@ -66,19 +51,6 @@ public class AccountDAO {
 			persistAccountInDb(em, account);
 		} finally {
 			em.close();
-		}
-	}
-
-	private void persistAccountInDb(EntityManager em, Account account) {
-		EntityTransaction et = em.getTransaction();
-		try {
-			et.begin();
-			em.persist(account);
-			et.commit();
-		} finally {
-			if (et.isActive()) {
-				et.rollback();
-			}
 		}
 	}
 
@@ -97,6 +69,49 @@ public class AccountDAO {
 			}
 		} finally {
 			em.close();
+		}
+	}
+
+	public void deleteAccount(Long id) {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Account account = getAccountFromDb(em, id);
+			if (account != null) {
+				deleteAccountFromDb(em, account);
+			} else {
+				throw new NoResultException("Account with id [" + id
+						+ "] not found");
+			}
+		} finally {
+			em.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<Account> getAccountsFromDb(EntityManager em, int maxResults) {
+		Query q = em.createNamedQuery("getAllAccounts");
+		q.setMaxResults(maxResults);
+		List<Account> allAccounts = (List<Account>) q.getResultList();
+		return allAccounts;
+	}
+
+	private Account getAccountFromDb(EntityManager em, Long id) {
+		Query q = em.createNamedQuery("getAccountById");
+		q.setParameter("id", id);
+		Account account = (Account) q.getSingleResult();
+		return account;
+	}
+
+	private void persistAccountInDb(EntityManager em, Account account) {
+		EntityTransaction et = em.getTransaction();
+		try {
+			et.begin();
+			em.persist(account);
+			et.commit();
+		} finally {
+			if (et.isActive()) {
+				et.rollback();
+			}
 		}
 	}
 
@@ -129,21 +144,6 @@ public class AccountDAO {
 			if (et.isActive()) {
 				et.rollback();
 			}
-		}
-	}
-
-	public void deleteAccount(Long id) {
-		EntityManager em = emf.createEntityManager();
-		try {
-			Account account = getAccountFromDb(em, id);
-			if (account != null) {
-				deleteAccountFromDb(em, account);
-			} else {
-				throw new NoResultException("Account with id [" + id
-						+ "] not found");
-			}
-		} finally {
-			em.close();
 		}
 	}
 
