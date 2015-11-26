a9c8d95e2245defd28cdd6231af8f08b511d3b76
moisko <boris.r.borisov@gmail.com>
2015-11-26 09:40:51 +0200

    format account.js
    Change-Id: Ibfd3aa54b535f6923288be2878479e530489e93a

--- src/main/webapp/js/account/account.js
+++ src/main/webapp/js/account/account.js
@@ -88,79 +88,77 @@ $(document).ready(function() {
 	var dataTable = $("#accounts-table").dataTable({
 		"aaSorting" : [[ 3, "asc" ]],
 		"aoColumnDefs" : [
-				{"aTargets" : [3],// DATE OF BIRTH column
-				"mRender" : function(datetimeInMillis) {
-					return datetime.toLocalDateTimeString(datetimeInMillis);
+			{"aTargets" : [3],// DATE OF BIRTH column
+			"mRender" : function(datetimeInMillis) {
+				return datetime.toLocalDateTimeString(datetimeInMillis);
+			},
+			"sType" : "date-bg"
+		},
+			{"aTargets" : [4],// DELETE column
+			"bSortable" : false,
+			"mRender" : function(id) {
+				return constructDeleteLink(id);
+			}
+		}],
+		"fnHeaderCallback" : function(nHead, aData, iStart, iEnd, aiDisplay) {
+			nHead.getElementsByTagName("th")[0].innerHTML = (iEnd - iStart) + " Accounts";
+		},
+		"fnRowCallback" : function(nRow, aData, iStart, iEnd, aiDisplay) {
+			var id = $("td a", nRow).attr("id");
+			var tdElements = $("td", nRow);
+			var index = 0;
+			for(index = 0; index < tdElements.length; index++) {
+				var tdElement = tdElements[index];
+				switch(index) {
+					case 0:
+					tdElement.setAttribute("id", "firstName_" + id);
+					tdElement.setAttribute("class", "edit");
+					break;
+					case 1:
+					tdElement.setAttribute("id", "lastName_" + id);
+					tdElement.setAttribute("class", "edit");
+					break;
+					case 2:
+					tdElement.setAttribute("id", "email_" + id);
+					tdElement.setAttribute("class", "edit");
+					break;
+					case 3:
+					tdElement.setAttribute("id", "dateOfBirth_" + id);
+					tdElement.setAttribute("class", "edit");
+					break;
+					case 4:
+					tdElement.setAttribute("class", "delete");
+					break;
+					default:
+					break;
+				}
+			}
+		},
+		"fnDrawCallback" : function () {
+			$("#accounts-table tbody tr td:not(.delete)").editable("/user-accounts/accounts/update", {
+				"type" : "account",
+				"onblur" : "submit",
+				"callback" : function(updatedValue) {
+					function convertUpdatedValueToColumnType(column) {
+						if(column === 3) {// DELETE column
+							return parse(updatedValue);
+						}
+						return updatedValue;
+					};
+					var position = dataTable.fnGetPosition(this);
+					var row = position[0];
+					var column = position[1];
+					var aData = dataTable.fnGetData(row);
+					aData[column] = convertUpdatedValueToColumnType(column);
+					dataTable.fnUpdate(aData, row);
 				},
-				"sType" : "date-bg"
-			},
-				{"aTargets" : [4],// DELETE column
-				"bSortable" : false,
-				"mRender" : function(id) {
-					return constructDeleteLink(id);
+				"onerror" : function(settings, original, xhr) {
+					alert("Failed to update account.\nServer returned " + xhr.status + ": " + xhr.responseText);
+					original.reset();
 				}
-			}
-			],
-			"fnHeaderCallback" : function(nHead, aData, iStart, iEnd, aiDisplay) {
-				nHead.getElementsByTagName("th")[0].innerHTML = (iEnd - iStart) + " Accounts";
-			},
-			"fnRowCallback" : function(nRow, aData, iStart, iEnd, aiDisplay) {
-				var id = $("td a", nRow).attr("id");
-				var tdElements = $("td", nRow);
-				var index = 0;
-				for(index = 0; index < tdElements.length; index++) {
-					var tdElement = tdElements[index];
-					switch(index) {
-						case 0:
-						tdElement.setAttribute("id", "firstName_" + id);
-						tdElement.setAttribute("class", "edit");
-						break;
-						case 1:
-						tdElement.setAttribute("id", "lastName_" + id);
-						tdElement.setAttribute("class", "edit");
-						break;
-						case 2:
-						tdElement.setAttribute("id", "email_" + id);
-						tdElement.setAttribute("class", "edit");
-						break;
-						case 3:
-						tdElement.setAttribute("id", "dateOfBirth_" + id);
-						tdElement.setAttribute("class", "edit");
-						break;
-						case 4:
-						tdElement.setAttribute("class", "delete");
-						break;
-						default:
-						break;
-					}
-				}
-			},
-			"fnDrawCallback" : function () {
-				$("#accounts-table tbody tr td:not(.delete)").editable("/user-accounts/accounts/update", {
-					"type" : "account",
-					"onblur" : "submit",
-					"callback" : function(updatedValue) {
-						function convertUpdatedValueToColumnType(column) {
-							if(column === 3) {// DELETE column
-								return parse(updatedValue);
-							}
-							return updatedValue;
-						};
-
-						var position = dataTable.fnGetPosition(this);
-						var row = position[0];
-						var column = position[1];
-						var aData = dataTable.fnGetData(row);
-						aData[column] = convertUpdatedValueToColumnType(column);
-						dataTable.fnUpdate(aData, row);
-					},
-					"onerror" : function(settings, original, xhr) {
-						alert("Failed to update account.\nServer returned " + xhr.status + ": " + xhr.responseText);
-						original.reset();
-					}
-				});
-			}
-		});
+			});
+		}
+	});
 
 	accountsTable.populateAccountsTable(dataTable);
 
